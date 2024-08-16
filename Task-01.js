160ed510c2fb123f40f64534c6cfadf2844d7d72
Simon Tadesse <simonzagwe@gmail.com>
2024-08-16 09:16:58 -0800

    Prodigy task 01

---
+++ Task-01.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+    if (window.scrollY > 50) {
+        nav.classList.add("scrolled-nav");
+    } else {
+        nav.classList.remove("scrolled-nav");
+    }
+});
